bb089535afed782e1b22f6eb57f7942288fc133d
Christina Warren <44888733+ce-warren@users.noreply.github.com>
2025-06-08 18:49:49 -0700

    fix ballot sorting

--- client/src/app/site/pages/meetings/pages/assignments/pages/assignment-polls/components/assignment-poll-detail/assignment-poll-detail.component.ts
+++ client/src/app/site/pages/meetings/pages/assignments/pages/assignment-polls/components/assignment-poll-detail/assignment-poll-detail.component.ts
@@ -87,9 +87,14 @@ export class AssignmentPollDetailComponent
                             ? optionContent?.getShortenedTitle(40)
                             : (optionContent?.getShortName() ?? this.translate.instant(`Deleted user`));
                         votes[token].votes.push(
-                            (pollOptions.length === 1 ? `` : `${candidate_name}: `) +
-                            `${this.voteValueToLabel(vote.value)}`
+                            this.poll.pollmethod === PollMethod.STV
+                                ? `${this.voteValueToLabel(vote.value)}` + '. ' + `${candidate_name}`
+                                : (pollOptions.length === 1 ? `` : `${candidate_name}: `) +
+                                    `${this.voteValueToLabel(vote.value)}`
                         );
+                        if (this.poll.pollmethod === PollMethod.STV) {
+                            votes[token].votes.sort()
+                        }
                     }
                 }
             }
